f54a426a709c56efda196b0d2a87a2cb082a0ea0
Luke1982 <gcx3@hotmail.com>
2018-11-23 15:35:14 +0100

    Only do the whole thing when productid is a 'Product'

--- modules/InventoryExtras/handlers/AfterInvDetSave.php
+++ modules/InventoryExtras/handlers/AfterInvDetSave.php
@@ -14,26 +14,29 @@ Class AfterInvDetSave extends VTEventHandler {
 			$invext_prefix = $invext->getPrefix();
 
 			$related_type = getSalesEntityType($invdet_data['related_to']);
-			$sibl = $invext->getSiblingFromInvoice($invdet_data['related_to'], $invdet_data['productid']);
+			$related_item = getSalesEntityType($invdet_data['productid']);
 
-			if ($related_type == 'Invoice' && !!$sibl && ($sibl[$invext_prefix . 'inv_sibling'] == '' || $sibl[$invext_prefix . 'inv_sibling'] == '0')) {
-				// This line is related to an invoice and a sibling was found. The sibling does not have
-				// a related inventorydetails line yet
-				$invext->updateInvDetRec($sibl['id'], $sibl['quantity'], $invdet_id, $invdet_data['quantity']);			
-			} else if ($related_type == 'Invoice' && !!$sibl && ($sibl[$invext_prefix . 'inv_sibling'] != '' || $sibl[$invext_prefix . 'inv_sibling'] != '0')) {
-				// Sibling was found, but it already has a related line
-				if ($sibl[$invext_prefix . 'inv_sibling'] == $invdet_id) {
-					// Already related to this line, update to be sure
-					$invext->updateInvDetRec($sibl['id'], $sibl['quantity'], $invdet_id, $invdet_data['quantity']);
+			if ($related_item == 'Products') {
+				$sibl = $invext->getSiblingFromInvoice($invdet_data['related_to'], $invdet_data['productid']);
+
+				if ($related_type == 'Invoice' && !!$sibl && ($sibl[$invext_prefix . 'inv_sibling'] == '' || $sibl[$invext_prefix . 'inv_sibling'] == '0')) {
+					// This line is related to an invoice and a sibling was found. The sibling does not have
+					// a related inventorydetails line yet
+					$invext->updateInvDetRec($sibl['id'], $sibl['quantity'], $invdet_id, $invdet_data['quantity']);			
+				} else if ($related_type == 'Invoice' && !!$sibl && ($sibl[$invext_prefix . 'inv_sibling'] != '' || $sibl[$invext_prefix . 'inv_sibling'] != '0')) {
+					// Sibling was found, but it already has a related line
+					if ($sibl[$invext_prefix . 'inv_sibling'] == $invdet_id) {
+						// Already related to this line, update to be sure
+						$invext->updateInvDetRec($sibl['id'], $sibl['quantity'], $invdet_id, $invdet_data['quantity']);
+					}
+				}
+
+				if ($related_type == 'SalesOrder') {
+					// Update the related product field with the summ of all invoice lines
+					$qty_in_order_tot = $invext->getQtyInOrderByProduct($invdet_data['productid']);
+					$invext->updateProductQtyInOrder($invdet_data['productid'], $qty_in_order_tot);
 				}
 			}
-
-			if ($related_type == 'SalesOrder') {
-				// Update the related product field with the summ of all invoice lines
-				$qty_in_order_tot = $invext->getQtyInOrderByProduct($invdet_data['productid']);
-				$invext->updateProductQtyInOrder($invdet_data['productid'], $qty_in_order_tot);
-			}			
-
 		}
 	}
 }
