1ccbd272d11a58edc01544916279cca2d5ea8c84
Luke1982 <gcx3@hotmail.com>
2018-11-26 15:06:22 +0100

    Correct current_user variable names for currency separator

--- modules/InventoryExtras/ProductsInBackOrderOnWidget.php
+++ modules/InventoryExtras/ProductsInBackOrderOnWidget.php
@@ -35,5 +35,5 @@ while ($line = $adb->fetch_array($r)) {
 $smarty->assign('lines', $lines);
 $smarty->assign('user_decnum', $current_user->column_fields['no_of_currency_decimals']);
 $smarty->assign('user_grpsep', $current_user->column_fields['currency_grouping_separator']);
-$smarty->assign('user_cursep', $current_user->column_fields['currency_currency_separator']);
+$smarty->assign('user_cursep', $current_user->column_fields['currency_decimal_separator']);
 $smarty->display('modules/InventoryExtras/ProductsInBackOrderOnWidget.tpl');
