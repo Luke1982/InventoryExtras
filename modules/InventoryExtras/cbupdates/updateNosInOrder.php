931e77734f2643be3eeb4453440fe3b840553209
Luke1982 <gcx3@hotmail.com>
2018-11-28 18:15:40 +0100

    Correct update qty's in order query

--- modules/InventoryExtras/cbupdates/updateNosInOrder.php
+++ modules/InventoryExtras/cbupdates/updateNosInOrder.php
@@ -26,7 +26,7 @@ class updateNosInOrder extends cbupdaterWorker {
 			$this->sendMsg('Changeset '.get_class($this).' already applied!');
 		} else {
 
-			$adb->query("UPDATE vtiger_inventorydetails INNER JOIN vtiger_salesorder ON vtiger_inventorydetails.related_to = vtiger_salesorder.salesorderid SET vtiger_inventorydetails.invextras_qty_in_order = (vtiger_inventorydetails.units_delivered_received - vtiger_inventorydetails.quantity)");
+			$adb->query("UPDATE vtiger_inventorydetails INNER JOIN vtiger_salesorder ON vtiger_inventorydetails.related_to = vtiger_salesorder.salesorderid SET vtiger_inventorydetails.invextras_qty_in_order = (vtiger_inventorydetails.quantity - vtiger_inventorydetails.units_delivered_received)");
 
 			$r = $adb->query("SELECT vtiger_inventorydetails.related_to, vtiger_inventorydetails.inventorydetailsid AS id 
 				              FROM vtiger_inventorydetails 
