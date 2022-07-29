2f7e5e9bd55aacab17bac71352a758decf086903
Luke1982 <gcx3@hotmail.com>
2022-07-29 10:48:12 +0200

    Copy workflow creation to update handler since addEntityMethod checks for doubles

--- modules/InventoryExtras/InventoryExtras.php
+++ modules/InventoryExtras/InventoryExtras.php
@@ -151,6 +151,7 @@ class InventoryExtras {
 			$this->doInstallcbUpdates();
 			$this->doRemoveInvExtrasBlockInInvendet();
 			$this->removeInvenExtrasEventHandlers();
+			$this->doCreateWorkflowFunction();
 		}
 	}
 
